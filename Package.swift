4ed91ba649f8fd69c4be76ca9a0b42ace9b7e6b6
Sammy Khamis <skhamis@mozilla.com>
2021-08-23 16:42:55 -1000

    testing adding fxa crate as a swift package

--- Package.swift
+++ Package.swift
@@ -11,14 +11,12 @@ let package = Package(
         .library(name: "CrashTest", targets: ["CrashTest"]),
         // TODO: more of our components here, once they support M1 builds.
         //.library(name: "Logins", targets: ["Logins"]),
-        //.library(name: "FxAClient", targets: ["FxAClient"]),
+        .library(name: "FxAClient", targets: ["FxAClient"]),
     ],
     dependencies: [
         // TODO: ship Glean via this same bundle?
         .package(name: "Glean", url: "https://github.com/mozilla/glean-swift", from: "39.0.4"),
-        // TODO: this external dependency is required for FxAClient,
-        // leaving it here as an example for now.
-        //.package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
+        .package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
     ],
     targets: [
         /*
@@ -76,10 +74,23 @@ let package = Package(
         //    dependencies: ["MozillaRustComponentsWrapper", "Sync15"],
         //    path: "external/application-services/components/logins/ios"
         //),
-        //.target(
-        //    name: "FxAClient",
-        //    dependencies: ["MozillaRustComponentsWrapper", "SwiftKeychainWrapper"],
-        //    path: "external/application-services/components/fxa-client/ios"
-        //),
+        .target(
+           name: "FxAClient",
+           dependencies: ["MozillaRustComponents", "SwiftKeychainWrapper"],
+           path: "generated/fxa-client"
+        ),
+        .binaryTarget(
+            name: "MozillaRustComponents",
+            //
+            // For release artifacts, reference the MozillaRustComponents as a URL with checksum.
+            //
+            url: "https://111955-129966583-gh.circle-artifacts.com/0/dist/MozillaRustComponents.xcframework.zip",
+            checksum: "f6807476ab4dd850290f4f87850719dfb9601deaee42b202ff8860480bdf1a42"
+            //
+            // For local testing, you can point at an (unzipped) XCFramework that's part of the repo.
+            // Note that you have to actually check it in and make a tag for it to work correctly.
+            //
+            //path: "./MozillaRustComponents.xcframework"
+        )
     ]
 )
