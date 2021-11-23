0c71a476430b2c827fc594ed4285629a4a33689a
Sammy Khamis <skhamis@mozilla.com>
2021-11-23 13:37:33 -1000

    adjusted update script to be easier to read

--- automation/update_package_swift.py
+++ automation/update_package_swift.py
@@ -2,6 +2,8 @@ import os
 from github import Github
 import hashlib
 import requests
+import fileinput
+import sys
 
 GITHUB_REPO = "mozilla/application-services"
 XC_FRAMEWORK_NAME = "MozillaRustComponents.xcframework.zip"
@@ -12,7 +14,7 @@ as_version = os.getenv("AS_VERSION")
 def get_xcframework_artifact():
     g = Github(github_access_token)
     repo = g.get_repo(GITHUB_REPO)
-    
+
     release = repo.get_release(as_version)
     for asset in release.get_assets():
         if asset.name == XC_FRAMEWORK_NAME:
@@ -22,38 +24,18 @@ def compute_checksum(xc_framework_url):
     req = requests.get(xc_framework_url)
     return hashlib.sha256(req.content).hexdigest()
 
-def update_package_swift(xc_framework_url, checksum):
-    old_url = ''
-    old_checksum = ''
-    with open(PACKAGE_SWIFT) as fp:
-        line = fp.readline()
-        while line:
-            line = fp.readline()
-            
-            # If this is the line that has the URL to
-            # the xcframework
-            if XC_FRAMEWORK_NAME in line:
-                url_start = line.find('"') + 1
-                url_end = line.find('"', url_start)
-                old_url = line[url_start:url_end]
-                # NOTE: We assume that the next line is the 
-                # checksum, there is a note in Package.swift
-                # to make sure we don't change that
-                line = fp.readline()
-                checksum_start = line.find('"') + 1
-                checksum_end = line.find('"', checksum_start)
-                old_checksum = line[checksum_start:checksum_end]
-                break
-    file = open(PACKAGE_SWIFT, "r+")
-    data = file.read()
-    data = data.replace(old_url, xc_framework_url)
-    data = data.replace(old_checksum, checksum)
-    file.close()
-    file = open(PACKAGE_SWIFT, "wt")
-    file.write(data)
-    file.close()
+def update_package_swift(as_version, checksum):
+    version_line = "let version ="
+    checksum_line = "let checksum ="
 
-
+    for line in fileinput.input(PACKAGE_SWIFT, inplace=1):
+        if line.strip().startswith(version_line):
+            # Replace the line with the new version included
+            line = f"{version_line} \"{as_version}\"\n"
+        elif line.strip().startswith(checksum_line):
+            # Replace the line with the new computed checksum
+            line = f"{checksum_line} \"{checksum}\"\n"
+        sys.stdout.write(line)
 def main():
     '''
     Updates `Package.swift` with the latest
@@ -63,7 +45,7 @@ def main():
         exit(1)
     xc_framework_url = get_xcframework_artifact()
     checksum = compute_checksum(xc_framework_url)
-    update_package_swift(xc_framework_url, checksum)
+    update_package_swift(as_version, checksum)
 
 
 
