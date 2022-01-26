dd4a2d5be1abc5bcb6a8bea7c798540d08d4ce1c
Firefox Sync Engineering <sync-team@mozilla.com>
2022-01-26 15:08:56 +0000

    Version 90.0.1

--- generated/logins/Generated/Metrics.swift
+++ generated/logins/Generated/Metrics.swift
@@ -22,7 +22,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2022, month: 1, day: 22, hour: 2, minute: 51, second: 52))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2022, month: 1, day: 26, hour: 15, minute: 8, second: 51))
     }
 
     enum LoginsStoreMigration {
