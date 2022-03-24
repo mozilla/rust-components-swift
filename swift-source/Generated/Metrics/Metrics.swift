3d6c15367b299004fa72f476a6fe4240bf3538ca
Firefox Sync Engineering <sync-team@mozilla.com>
2022-03-24 15:08:06 +0000

    Version 91.1.1

--- swift-source/Generated/Metrics/Metrics.swift
+++ swift-source/Generated/Metrics/Metrics.swift
@@ -22,7 +22,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2022, month: 2, day: 16, hour: 17, minute: 5, second: 28))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2022, month: 3, day: 24, hour: 15, minute: 6, second: 27))
     }
 
     enum NimbusEvents {
