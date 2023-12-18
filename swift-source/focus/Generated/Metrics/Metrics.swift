be795798e3dfe6d1b376fa0ba687fc3a6eb9909b
Firefox Sync Engineering <sync-team@mozilla.com>
2023-12-18 11:23:34 -0500

    Nightly auto-update (122.0.0)

--- swift-source/focus/Generated/Metrics/Metrics.swift
+++ swift-source/focus/Generated/Metrics/Metrics.swift
@@ -25,7 +25,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2023, month: 12, day: 16, hour: 5, minute: 28, second: 27))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2023, month: 12, day: 18, hour: 16, minute: 3, second: 14))
     }
 
     enum NimbusEvents {
