d567fe5f6ec2764ecf945dcb5345bbf1e2a94094
Tarik Eshaq <teshaq@mozilla.com>
2024-01-22 11:23:20 -0500

    Cuts v123.0.0

--- swift-source/focus/Generated/Metrics/Metrics.swift
+++ swift-source/focus/Generated/Metrics/Metrics.swift
@@ -25,7 +25,7 @@ extension GleanMetrics {
             // Intentionally left private, no external user can instantiate a new global object.
         }
 
-        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2024, month: 1, day: 20, hour: 5, minute: 20, second: 38))
+        public static let info = BuildInfo(buildDate: DateComponents(calendar: Calendar.current, timeZone: TimeZone(abbreviation: "UTC"), year: 2024, month: 1, day: 22, hour: 15, minute: 14, second: 30))
     }
 
     enum NimbusEvents {
